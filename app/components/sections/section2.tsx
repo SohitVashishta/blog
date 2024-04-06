fc3974adba4ce3fad7d8e33d50d5c2b681ea7b3a
Sohit Vashishtha <sohitvashishtha777@gmail.com>
2024-04-07 01:36:54 +0530

    Update section2.tsx

--- app/components/sections/section2.tsx
+++ app/components/sections/section2.tsx
@@ -7,7 +7,7 @@ export default function Section2() {
                         <div className="col-lg-4">
                             <div className="post-entry-1 lg">
                                 <a href="single-post.html"><img src="/assert/img/IMG_20210321_072817.jpg" alt="" className="img-fluid" /></a>
-                                <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                                <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
                                 <h2><a href="single-post.html">Savoring the tranquility of a misty morning in Mussoorie with a cup of steaming tea. ☕️🍃 </a></h2>
                                 <p className="mb-4 d-block">Nestled in the embrace of Mussoorie's misty mornings, there's a special magic in sipping tea as the world wakes up. With each gentle sip, the tranquil beauty of the Himalayas whispers its secrets, inviting me to pause, breathe, and embrace the serenity of the moment. ☕️🏔️ #MorningBliss</p>
 
@@ -21,29 +21,29 @@ export default function Section2() {
                                 <div className="col-lg-4 border-start custom-border">
                                     <div className="post-entry-1">
                                         <a href="single-post.html"><img src="/assert/img/YamunotriTemple.jpg" alt="" className="img-fluid" /></a>
-                                        <div className="post-meta"><span className="date">Sport</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                                        <div className="post-meta"><span className="date">Sport</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
                                         <h2><a href="single-post.html">"Exploring the divine beauty and spiritual serenity of Yamunotri Dham, where every step feels like a sacred journey and every moment is imbued with tranquility. 🙏🌿 #YamunotriDham #DivineJourney"</a></h2>
                                     </div>
                                     <div className="post-entry-1">
                                         <a href="single-post.html"><img src="/assert/img/26322.jpg" alt="" className="img-fluid" /></a>
-                                        <div className="post-meta"><span className="date">Food</span> <span className="mx-1">&bullet;</span> <span>Jul 17th '22</span></div>
+                                        <div className="post-meta"><span className="date">Food</span> <span className="mx-1"></span> <span>Jul 17th 22</span></div>
                                         <h2><a href="single-post.html">"Embracing the divine essence of Vrindavan Dham, where every corner resonates with the eternal love of Radha and Krishna. 🌸🙏 #VrindavanDham #DivineLove"</a></h2>
                                     </div>
                                     <div className="post-entry-1">
                                         <a href="single-post.html"><img src="/assert/img/26512.jpg" alt="" className="img-fluid" /></a>
-                                        <div className="post-meta"><span className="date">Design</span> <span className="mx-1">&bullet;</span> <span>Mar 15th '22</span></div>
+                                        <div className="post-meta"><span className="date">Design</span> <span className="mx-1"></span> <span>Mar 15th 22</span></div>
                                         <h2><a href="single-post.html">"Bathed in the soft glow of dawn, the mountains stand sentinel to a new day's beginning."</a></h2>
                                     </div>
                                 </div>
                                 <div className="col-lg-4 border-start custom-border">
                                     <div className="post-entry-1">
                                         <a href="single-post.html"><img src="/assert/img/64124.jpg" alt="" className="img-fluid" /></a>
-                                        <div className="post-meta"><span className="date">Business</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                                        <div className="post-meta"><span className="date">Business</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
                                         <h2><a href="single-post.html">"Where the sacred waters of the Yamuna flow pure and eternal, Yamunotri Dham beckons with its serene beauty and spiritual resonance, a sanctuary for the soul seekers. 🕉️💧 #YamunotriDham #SpiritualOasis"</a></h2>
                                     </div>
                                     <div className="post-entry-1">
                                         <a href="single-post.html"><img src="/assert/img/64131.jpg" alt="" className="img-fluid" /></a>
-                                        <div className="post-meta"><span className="date">Tech</span> <span className="mx-1">&bullet;</span> <span>Mar 1st '22</span></div>
+                                        <div className="post-meta"><span className="date">Tech</span> <span className="mx-1"></span> <span>Mar 1st 2</span></div>
                                         <h2><a href="single-post.html">As the sun dips below the horizon, painting the sky in hues of fiery gold, we're reminded that even endings can be breathtakingly beautiful. 🌅 #SunsetSplendor"</a></h2>
                                     </div>
                                    
