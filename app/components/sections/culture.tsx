44b3d6ffeb41acbfdbea8e04e4b3137282195f48
Sohit Vashishtha <sohitvashishtha777@gmail.com>
2024-04-07 01:33:13 +0530

    Update culture.tsx

--- app/components/sections/culture.tsx
+++ app/components/sections/culture.tsx
@@ -17,7 +17,7 @@ export default function Culture(){
                 <img src="assert/img/IMG_20210321_105039.jpg" alt="" className="img-fluid"/>
               </a>
               <div>
-                <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th </span></div>
                 <h3><a href="single-post.html">Sacred serenity in Manali's heart, where Hidimba Devi's temple whispers tales of ancient devotion amidst towering cedars.</a></h3>
                 <p>The Hidimba Devi Temple, nestled in the picturesque hills of Manali, Himachal Pradesh, is a sacred shrine dedicated to Hidimba Devi, a character from the Indian epic Mahabharata.
                    Constructed in 1553, this unique temple stands out for its exquisite wooden architecture, showcasing intricate carvings and a pagoda-style roof. Surrounded by dense cedar forests, the temple offers a tranquil oasis for spiritual seekers and visitors alike. Legend has it that Hidimba Devi meditated here, seeking penance for her past deeds, making it a place of immense significance in Hindu mythology. Today, it remains a symbol of devotion and cultural heritage, drawing pilgrims and tourists to experience its serene ambiance and divine connection.</p>
@@ -29,7 +29,7 @@ export default function Culture(){
               <div className="col-lg-4">
                 <div className="post-entry-1 border-bottom">
                   <a href="single-post.html"><img src="assert/img/IMG_20210322_115302.jpg" alt="" className="img-fluid"/></a>
-                  <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                  <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th </span></div>
                   <h2 className="mb-2"><a href="single-post.html">Exploring the hidden gems of Mussoorie, where the mystical Vysh Nadi whispers secrets
                    of nature's allure amidst the Himalayan foothills. 🌿💧 #VyshNadi #MussoorieMystique"</a></h2>
                   <span className="author mb-3 d-block">n</span>
@@ -41,7 +41,7 @@ export default function Culture(){
               <div className="col-lg-8">
                 <div className="post-entry-1">
                   <a href="single-post.html"><img src="assert/img/IMG_20210322_164148_1.jpg" alt="" className="img-fluid"/></a>
-                  <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+                  <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
                   <h2 className="mb-2"><a href="single-post.html">Riverside reverie in Mussoorie: Where the tranquil flow mirrors nature's poetry, and every moment is a gentle embrace of serenity. 🌊🍃 #MussoorieMagic</a></h2>
                   <span className="author mb-3 d-block"></span>
                   <p className="mb-4 d-block"></p>
@@ -52,37 +52,37 @@ export default function Culture(){
 
           <div className="col-md-3">
             <div className="post-entry-1 border-bottom">
-              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
               <h2 className="mb-2"><a href="single-post.html">How to Avoid Distraction and Stay Focused During Video Calls?</a></h2>
               <span className="author mb-3 d-block">Jenny Wilson</span>
             </div>
 
             <div className="post-entry-1 border-bottom">
-              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
               <h2 className="mb-2"><a href="single-post.html">17 Pictures of Medium Length Hair in Layers That Will Inspire Your New Haircut</a></h2>
               <span className="author mb-3 d-block">Jenny Wilson</span>
             </div>
 
             <div className="post-entry-1 border-bottom">
-              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
               <h2 className="mb-2"><a href="single-post.html">9 Half-up/half-down Hairstyles for Long and Medium Hair</a></h2>
               <span className="author mb-3 d-block">Jenny Wilson</span>
             </div>
 
             <div className="post-entry-1 border-bottom">
-              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
               <h2 className="mb-2"><a href="single-post.html">Life Insurance And Pregnancy: A Working Mom’s Guide</a></h2>
               <span className="author mb-3 d-block">Jenny Wilson</span>
             </div>
 
             <div className="post-entry-1 border-bottom">
-              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
               <h2 className="mb-2"><a href="single-post.html">The Best Homemade Masks for Face (keep the Pimples Away)</a></h2>
               <span className="author mb-3 d-block">Jenny Wilson</span>
             </div>
 
             <div className="post-entry-1 border-bottom">
-              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
+              <div className="post-meta"><span className="date">Culture</span> <span className="mx-1"></span> <span>Jul 5th 22</span></div>
               <h2 className="mb-2"><a href="single-post.html">10 Life-Changing Hacks Every Working Mom Should Know</a></h2>
               <span className="author mb-3 d-block">Jenny Wilson</span>
             </div>
